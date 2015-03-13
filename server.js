3e582eb5e8a2a5105446b3b701e689aec62df29e
Marc Arbesman <marbesman@gmail.com>
2015-03-13 13:37:09 -0400

    400 errors send back Json object instead of tex

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Heroku defines the environment variable PORT, and requires the binding address to be 0.0.0.0
-var host = process.env.PORT ? '0.0.0.0' : '10.207.64.173';
+//var host = process.env.PORT ? '0.0.0.0' : '10.207.64.173';
+var host = process.env.PORT ? '0.0.0.0' : '10.4.65.165';
 var port = process.env.PORT || 1337;
 
 var cors_proxy = require('./lib/cors-anywhere');
